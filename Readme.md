4dde4d77d72fd45a7cdb993a08ffc1b580f844c6
NeiltonSeguins <seguins.neilton@gmail.com>
2022-04-01 15:00:04 -0300

    Primeiro commit

---
+++ Readme.md
@@ -0,0 +1,22 @@
+# Banco de dados com Spreashets do Google
+
+Exemplo simples e bem básico de como usar as planilhas do Google como um Banco de dados. No arquivo index.js você pode ver como fazer requisições GET, POST e DELETE usando fetchAPI. Tentei deixar o código mais didático possível até para quem está começando com Javascript.
+
+## Link do service:
+
+- [SheetDB](https://sheetdb.io/)
+
+## Vídeo explicativo de como usar usando Axios
+
+- [Uma API para Preencher Planilhas!](https://www.youtube.com/watch?v=c_ZxPy5pWZ0)
+
+## Link da Documentação
+
+- [SheetDB API](https://docs.sheetdb.io/?html--javascript#introduction)
+
+## Tecnologias
+- HTML
+- CSS
+- Javascript
+- fetchAPI
+- SheetDB
---
+++ index.js
@@ -0,0 +1,89 @@
+const url = 'https://sheetdb.io/api/v1/isumaaox3cavy/'; //Você pode testar sua API aqui
+
+// =================================================================================================
+
+// Dados
+const data = {
+  nome: 'Baby Yoda',
+  preco: 150,
+  categoria: 'Starwars',
+  imagem_url: 'https://m.media-amazon.com/images/I/71oXYSV2lEL._AC_SY450_.jpg',
+};
+
+// =================================================================================================
+
+// Método GET
+async function pegaProduto(url) {
+  const resp = await fetch(url);
+  const jsonBody = await resp.json();
+  return montaHTML(jsonBody);
+}
+pegaProduto(url);
+
+// =================================================================================================
+
+// Método POST
+async function adicionaProduto(url, data) {
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  return res;
+}
+// adicionaProduto(url, data);
+
+// =================================================================================================
+
+// Método DELETE
+async function deletaProduto(url, coluna, valor) {
+  const res = await fetch(`${url}/${coluna}/${valor}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  return console.log(res.json());
+}
+
+// deletaProduto(url, 'preco', 200);
+
+// =================================================================================================
+
+//Função que monta o HTML da página
+function montaHTML(dadosDB) {
+  dadosDB.map((produto) => {
+    // captura a tag ul do html
+    const listaDeProdutos = document.querySelector('#lista');
+
+    // Cria os elementos
+    const item = document.createElement('li');
+    const titulo = document.createElement('p');
+    const preco = document.createElement('span');
+    const categoria = document.createElement('h2');
+    const img = document.createElement('img');
+
+    //Adicona classes css aos elementos criados
+    item.classList.add('produtos__card');
+    img.classList.add('produtos__img');
+    titulo.classList.add('produtos__description');
+    preco.classList.add('produtos__valor');
+    categoria.classList.add('produtos__valor');
+
+    // Preenche os elementos html com os dados da API
+    titulo.innerHTML = `${produto.nome}`;
+    preco.innerHTML = `${produto.preco}`;
+    categoria.innerHTML = `${produto.categoria}`;
+    img.src = `${produto.imagem_url}`;
+
+    // Monta o HTML para cada Item
+    listaDeProdutos.appendChild(item);
+    item.appendChild(img);
+    item.appendChild(categoria);
+    item.appendChild(titulo);
+    item.appendChild(preco);
+  });
+}
